9da94d6b40e613014ad42de78297d584b61c1f93
MUHAMMAD SHAMMAS <shammasp888@gmail.com>
2024-10-03 14:59:11 +0530

    Delete toapp directory

--- toapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Customer(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic=models.ImageField(upload_to='profile_pic/customerprofilepic/')
-    address=models.CharField(max_length=40)
-    mobile=models.IntegerField(max_length=10,null=False)
